d27eb59dc3b8374400e648ad98d462ed13ab4e85
jayash-gupta <er.jayashgupta@gmail.com>
2024-01-18 23:38:03 +0530

    Updating App

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5000",
+    origin: "*",
     // credentials: true,
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
