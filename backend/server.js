c740d209e5c958476f120d20aeddbf688d83e878
jayash-gupta <er.jayashgupta@gmail.com>
2024-01-27 00:07:54 +0530

    Updating

--- backend/server.js
+++ backend/server.js
@@ -23,20 +23,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-  console.log("Yeahhhhhhhhh");
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
@@ -47,20 +46,16 @@ app.use(errorHandler);
 const PORT = process.env.PORT;
 
 const server = app.listen(
-  PORT,
+  5000,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     // credentials: true,
-    methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
-    credentials: true,
   },
-  allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
@@ -94,5 +89,3 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-
-//updating...
